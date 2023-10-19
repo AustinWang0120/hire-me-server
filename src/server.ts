6ee9b75b8c7e42c1ea32daace17eee4886044ae1
Austin Wang <yyaustin@bu.edu>
2023-10-18 23:45:28 -0400

    Setup typescript, eslint, prettier

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import type { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, HireMe!");
+});
+
+const PORT_NUMBER = parseInt(process.env.PORT as string, 10);
+const PORT: number = isNaN(PORT_NUMBER) ? 3001 : PORT_NUMBER;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
